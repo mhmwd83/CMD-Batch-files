d1bfcdc19487cf4fe15123bd3db23a2790f4ec85
Mahmoud Gamal <Mahmoud_8383@rocketmail.com>
2022-07-18 15:29:18 +0200

    up

---
+++ Title.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+:: This is a comment that you can write in batch script.
+
+Title Mahmoud Gamal
+
+:: Title is the cmd window name.
+
+ECHO Hello Appuals Users, this is a simple batch script.
+
+PAUSE
