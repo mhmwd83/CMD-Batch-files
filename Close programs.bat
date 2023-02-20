995ad63ebaf51642068adbf010753e01b5c284ca
Mahmoud Gamal <Mahmoud_8383@rocketmail.com>
2023-02-20 09:05:28 +0200

    updat-close-prog

--- Close programs.bat
+++ Close programs.bat
@@ -2,5 +2,8 @@ taskkill /F /IM chrome.exe /T > nul
 taskkill /F /IM OUTLOOK.EXE /T > nul
 taskkill /F /IM X2.EXE /T > nul
 taskkill /F /IM Teams.exe /T > nul
-
-@pause
+#@pause
+#shutdown
+shutdown /s /f /t 0
+#Restart
+#shutdown.exe /r /t 00   
